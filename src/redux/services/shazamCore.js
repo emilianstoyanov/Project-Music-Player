9a3830c88bc497dd1d1757a7a3bd55f0db15e359
Emilian Stoyanov <emilian.stoyanov@outlook.com>
2023-08-16 12:20:03 +0300

    createig a GET request to the shazam api

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,12 @@
+const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': '073e99107emsh2b56c736be21295p16d35ejsn20e7213472fa',
+      'X-RapidAPI-Host': 'shazam-core.p.rapidapi.com'
+    }
+  };
+  
+  fetch('https://shazam-core.p.rapidapi.com/v1/charts/world', options)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
