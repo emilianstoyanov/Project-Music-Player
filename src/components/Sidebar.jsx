364d962639794285f8bb6153258cc9b87daf2f0d
Emilian Stoyanov <emilian.stoyanov@outlook.com>
2023-08-27 13:14:13 +0300

    adding a navigation bar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,25 @@
-import { useState } from "react";
-import { NavLink } from "react-router-dom";
+import { useState } from 'react';
+import { NavLink } from 'react-router-dom';
 import { RiCloseLine } from 'react-icons/ri';
 
-import { logo } from "../assets";
-// import { links } from '../assert/constants';
+import { logo } from '../assets';
+import { links } from '../assets/constants';
+
+const NavLinks = ({ handleClick }) => (
+  <div className="mt-10">
+    {links.map((item) => (
+      <NavLink 
+        key={item.name}
+        to={item.to}
+        className="flex flex-row justify-start items-center my-8 text-sm font-medium 
+        text-gray-400 hover:text-cyan-400"
+        onClick={() => handleClick && handleClick()}
+        >
+          {item.name}
+      </NavLink>
+    ))}
+  </div>
+);
 
 const Sidebar = () => {
   const [modileMenuOpen, setMobileMenuOpen] = useState(false);
@@ -12,6 +28,7 @@ const Sidebar = () => {
     <>
       <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#191624]">
         <img src={logo} alt="logo" className="w-full h-14 object-contain" />
+        <NavLinks />
       </div>
     </>
   )
