087202152938d0ced5ea53974a1b85e9180b1e89
Emilian Stoyanov <emilian.stoyanov@outlook.com>
2023-08-31 10:49:49 +0300

    adding on get song details

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -3,22 +3,23 @@ import { useSelector, useDispatch} from 'react-redux';
 import { DetailsHeader, Error, Loader, RelatedSongs } from "../components";
 
 import { setActiveSong, playPause } from "../redux/features/playerSlice";
+import { useGetSongDetailsQuery } from "../redux/services/shazamCore";
 
 const SongDetails = () => {
     const dispatch = useDispatch();
     const { songid } = useParams();
     const { activeSong, isPlaying } = useSelector((state) => state.player);
-
+    const { data: sondData, isFetching: isFetchingSongDetails } = useGetSongDetailsQuery({ songid });
 
     console.log(songid);
 
     return (
         <div className=" flex flex-co;">
-            {/* <DetailsHeader artistId={artistId} sondData={sondData} /> */}
+            <DetailsHeader artistId="" sondData={sondData} />
 
             <div className="mb-10">
                 <h2 className="text-white text-3xl font-bold">Lyrics:</h2>
-                
+
                 <div className="mt-5">
                 </div>
             </div>
