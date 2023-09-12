97e323dfd251c6880c84c89d6a60bcf6f29a3b1f
Emilian Stoyanov <emilian.stoyanov@outlook.com>
2023-09-12 11:15:40 +0300

    update artist details

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const shazamCoreApi = createApi({
     reducerPath: 'shazamCoreApi',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+        baseUrl: 'https://shazam-core.p.rapidapi.com/',
         prepareHeaders: (headers) => {
             headers.set('X-RapidAPI-Key', '073e99107emsh2b56c736be21295p16d35ejsn20e7213472fa');
 
@@ -14,11 +14,14 @@ export const shazamCoreApi = createApi({
         getTopCharts: builder.query({ query: () => '/charts/world' }),
         getSongDetails: builder.query({ query: ({ songid }) => `/tracks/details?track_id$=${songid}` }),
         getSongRelated: builder.query({ query: ({ songid }) => `/tracks/related?track_id$=${songid}` }),
+        getArtistDetails: builder.query({ query: (artistId) => `v2/artists/details?artist_id=${artistId}` }),
+
     }),
 });
 
 export const {
     useGetTopChartsQuery,
     useGetSongDetailsQuery,
-    useGetSongRelatedQuery,    
+    useGetSongRelatedQuery,  
+    useGetArtistDetailsQuery,  
 } = shazamCoreApi;
